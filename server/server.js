cdca303231fe6c2ca171abe1f4304b634452de41
Sarveswaran MG <sarves@Sarveswarans-MacBook-Air.local>
2025-03-18 23:18:22 +0530

    path added

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const app = express();
 const mongoSanitize = require("express-mongo-sanitize");
-
+const path = require("path");
 app.use(helmet());
 app.disable("x-powered-by");
 app.use(mongoSanitize());
